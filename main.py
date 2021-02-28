2bef9a2c2d4346c9de80a2c4669148afb32a5ee1
mateusz-saganowski <65487797+mateusz-saganowski@users.noreply.github.com>
2021-02-28 22:19:33 +0100

    commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-print("Wyświetlenie listy zrobionych zadań: 1")
-print("Wyświetlenie listy zadań do wykonania: 2")
-print("Dodaj zadanie: 3")
+print("1: Wyświetlenie listy zrobionych zadań")
+print("2: Wyświetlenie listy zadań do wykonania")
+print("3: Dodaj zadanie")
+print("0: Wyjdź")
 while True:
     odpowiedz = input('Wybierz menu ')
     if odpowiedz == "3":
         nowe_zadanie = input("Napisz jakie zadanie chcesz dodać: ")
+    if odpowiedz == "2":
         print(nowe_zadanie)
+    if odpowiedz == "0":
         break
